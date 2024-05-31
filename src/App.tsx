7d7d866ca5e42dbf853565027ccdc77b3cf440a8
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-05-31 16:12:17 -0300

    chore: Refactor Button component and move Notification component to separate directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,31 @@
-import React from 'react';
-import Button from './components/Button/Button';
-import Notification from './components/notifications/Notification';
-
+import React, { useState } from "react";
+import Button from "./components/Button/Button";
+import Notification from "./components/notifications/Notification";
 
 // URL de um ícone pequeno de exemplo
-const imageSrc = 'https://img.icons8.com/ios-filled/50/000000/react-native.png';
+const imageSrc = "https://img.icons8.com/ios-filled/50/000000/react-native.png";
 
-const App: React.FC = () => {
-
+function App() {
+  const [showNotification, setShowNotification] = useState(false);
 
   return (
     <div>
-
-
+      <Button onClick={() => setShowNotification(true)}>
+        <Button.Text>Mostrar notificação</Button.Text>
+        <Button.Image src={imageSrc} alt="Imagem" />
+      </Button>
+      {showNotification && (
         <Notification>
-            <Notification.Icon src={imageSrc} alt="Imagem" />
-            <Notification.Text>Notificação com imagem</Notification.Text>
-            <Notification.Tooltip>Notificação com imagem</Notification.Tooltip>
-            <Button>Botão</Button>
-            <Notification.Close onClick={() => alert('Notificação fechada!')} />
+          <Notification.Icon src={imageSrc} alt="Imagem" />
+          <Notification.Text>
+            Notificação com imagem, texto e tooltip
+          </Notification.Text>
+          <Notification.Tooltip>Tooltip da notificação</Notification.Tooltip>
+          <Notification.Close onClick={() => setShowNotification(false)} />
         </Notification>
-
-        <Notification>
-            <Notification.Text>Notificação apenas com texto</Notification.Text>
-            <Notification.Close onClick={() => alert('Notificação fechada!')} />
-        </Notification>
-
-        <Notification>
-            <Notification.Icon src={imageSrc} alt="Imagem" />
-            <Notification.Text>Notificação com imagem e texto</Notification.Text>
-            <Notification.Close onClick={() => alert('Notificação fechada!')} />
-        </Notification>
-
-        <Notification>
-            <Notification.Icon src={imageSrc} alt="Imagem" />
-            <Notification.Text>Notificação com imagem, texto e tooltip</Notification.Text>
-            <Notification.Tooltip>Tooltip da notificação</Notification.Tooltip>
-            <Notification.Close onClick={() => alert('Notificação fechada!')} />
-        </Notification>
-
-
+      )}
     </div>
   );
-};
+}
 
 export default App;
--- src/components/notifications/Styles.ts
+++ src/components/notifications/Styles.ts
@@ -1,20 +1,25 @@
 import styled from 'styled-components';
-import {StyledButton} from "../Button/Styles";
 import { NotificationProps } from '../../@types/Notification';
 
-export const StyledNotification = styled.div<NotificationProps >`
+
+
+export const StyledNotification = styled.div<NotificationProps>`
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 15px;
-    border-radius: 10px;
-    background-color: #f0f0f0;
+    border-radius: 15px;
+    background-color: #007BFF;
+    color: #fff; 
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-    width: ${props => props.width || 'auto'};
+    width: ${props => props.width || '300px'}; 
     height: ${props => props.height || 'auto'};
     font-size: ${props => props.fontSize || '1em'};
+    position: fixed; 
+    top: 50%; 
+    left: 50%; 
+    transform: translate(-50%, -50%); 
 `;
-
 
 export const StyledNotificationIcon = styled.img`
     width: 25px;
@@ -25,7 +30,7 @@ export const StyledNotificationIcon = styled.img`
 export const StyledNotificationText = styled.span`
     flex-grow: 1;
     font-size: 1em;
-    color: #333;
+    color: #fff; 
     font-weight: 500;
 `;
 
@@ -54,7 +59,7 @@ export const StyledNotificationClose = styled.button`
     background: none;
     border: none;
     font-size: 1.2em;
-    color: #333;
+    color: #fff; 
     cursor: pointer;
     transition: color 0.3s ease-in-out;
 
