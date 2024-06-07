c7233880f359e52f610ce82312545f94f03055aa
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-06-07 10:15:40 -0300

    Refactor CardContainer component to improve responsiveness and spacing

--- src/components/Card/CardContainer.tsx
+++ src/components/Card/CardContainer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import CountCard from './CountCard';
 
@@ -8,26 +7,36 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   gap: 20px;
-  margin: 50px;
-  
+  padding-top: 5rem;
+  margin-right: 100px;
+  width: 100%;
+
+  @media(max-width: 490px){
+    margin-right: 35rem;
+  }
+
   @media (min-width: 768px) {
-        display: flex;
-        flex-direction: column;
-    }
-    @media (min-width: 1024px) {
-    display: flex;
+    flex-direction: column;
+  }
+
+  @media (min-width: 1024px) {
     flex-direction: row;
-    
-}
+  }
+  
+`;
+
+const StyledCountCard = styled(CountCard)`
+  width: 100%;
+
 
 `;
 
 const CardContainer: React.FC = () => {
   return (
     <Container>
-      <CountCard name='Habilidades:' value={30} />
-      <CountCard name='Estress:' value={60} />
-      <CountCard name='Felicidade:'value={90} />
+      <StyledCountCard name='Habilidades:' value={30} /> 
+      <StyledCountCard name='Estress:' value={17} /> 
+      <StyledCountCard name='Felicidade:' value={90} /> 
     </Container>
   );
 };
