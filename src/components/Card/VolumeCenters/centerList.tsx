e236785e2ed3685a263963cab34b95bbdac0303c
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-06-18 18:00:56 -0300

    feat: add Volume Centers list component

---
+++ src/components/Card/VolumeCenters/centerList.tsx
@@ -0,0 +1,55 @@
+import IconIndustrie from '../../../assets/Industrie.png';
+
+
+export const centerList = [
+    {
+        icon:   IconIndustrie,
+        code: 'BS01',
+        Location: 'Formosa'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS02',
+        Location: 'Buriti'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS03',
+        Location: 'Formosa'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS04',
+        Location: 'Buriti'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS05',
+        Location: 'Formosa'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS06',
+        Location: 'Buriti'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS07',
+        Location: 'Formosa'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS08',
+        Location: 'Buriti'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS09',
+        Location: 'Formosa'
+    },
+    {
+        icon:   IconIndustrie,
+        code: 'BS10',
+        Location: 'Buriti'
+    }
+]
